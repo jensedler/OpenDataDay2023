a71391c837aeaee714102dc753ab3347ea93b364
Jens Edler <edler.jens@gmail.com>
2023-02-26 19:10:10 +0100

    Added Code for Bielefeld
    Room and Tileset added.

---
+++ maps/Code For Bielefeld.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Code For Bielefeld" tilewidth="32" tileheight="32" tilecount="6" columns="6">
+ <image source="../tilesets/code_for_bielefeld.png" width="192" height="32"/>
+</tileset>
